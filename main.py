4afe769929fbb4c12d9db2f9fa785a3fe74225a1
fl0rchus <quinterosflorencia0@gmail.com>
2021-07-14 18:47:56 -0300

    nuevas frases agregadas

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ TOKEN = os.environ.get('TOKEN')
 bot = commands.Bot(command_prefix="!")
 
 
-speakers = ["Ferfer", "murd0c", "Mati", "Luqui", "Nacho", "Flwerr"]
+speakers = ["Ferfer", "murd0c", "Mati", "Luqui", "Nacho", "Flwerr", "bri_96"]
 subject = ["Rust", "Node", "React", "Haskell", "Python",
            "GraphQL?", "Ingles ah no Moni no está jeje"]
 
 frases = ["Joya", "JAJAJA LPM", "chupala gato",
-          "ferlacion",  "eee no se, que se yo", "No se puede creer estos caras de remil verga", "Sos trolo?", "me quiero morir", "si si", "aham", "messirve"]
+          "ferlacion",  "eee no se, que se yo", "No se puede creer estos caras de remil verga", "Sos trolo?", "me quiero morir", "si si", "aham", "messirve", "FIUUUUMBA", "VALORES Y PRINCIPIOS", "COMO NO TE VAN A GUSTAR LOS SANGUCHITOS DE MIGA?", "ah re"]
 
 month = ["Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
 day = random.randint(1, 31)
 
 saludos = ["Buenos dias lucesitas", "Buendia Pimpollos",
-           "Arriba remolonxs que es un nuevo dia", "Levantensé parásitos miren la hora que es y ustedes durmiendo como cerdos", "Holo"]
+           "Arriba remolonxs que es un nuevo dia", "Levantensé parásitos miren la hora que es y ustedes durmiendo como cerdos", "Holo", "BRRRUENAS"]
 
 despedida = ["ay deberia estar durmiendo", "deberia estar trabajando xd"]
 
@@ -49,6 +49,11 @@ async def charla(ctx):
 @bot.command()
 async def issue(ctx):
     await ctx.send("No se aceptan issues")
+
+
+@bot.command()
+async def frases(ctx):
+    await ctx.send("deberia tirar un arreglo de frase ah re")
 
 
 @bot.event
